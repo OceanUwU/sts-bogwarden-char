1ab628c6729c62257021b2c095432682616844fc
Ocean <ocean@ocean.lol>
2023-09-26 19:17:26 +0100

    0.1.3

--- src/main/java/bogwarden/powers/Spines.java
+++ src/main/java/bogwarden/powers/Spines.java
@@ -17,36 +17,44 @@ import static bogwarden.BogMod.makeID;
 public class Spines extends AbstractBogPower {
     public static final String POWER_ID = makeID("Spines");
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    private static int HALF_ATTACK_DIV = 2;
 
     public Spines(AbstractCreature owner, int amount) {
         super(POWER_ID, powerStrings.NAME, PowerType.BUFF, false, owner, amount);
     }
     
     public void updateDescription() {
-        description = powerStrings.DESCRIPTIONS[0] + amount + powerStrings.DESCRIPTIONS[1];
+        description = powerStrings.DESCRIPTIONS[0] + amount + powerStrings.DESCRIPTIONS[1] + (amount / HALF_ATTACK_DIV) + powerStrings.DESCRIPTIONS[2];
     }
 
-    public void trigger(AbstractMonster m) {
+    public void trigger(AbstractMonster m, int div) {
         addToBot(new AbstractGameAction() {
             public void update() {
                 isDone = true;
                 flash();
             }
         });
-        addToBot(new DamageAction(m, new DamageInfo(owner, amount, DamageInfo.DamageType.THORNS), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
+        addToBot(new DamageAction(m, new DamageInfo(owner, amount / div, DamageInfo.DamageType.THORNS), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
     }
 
     @SpirePatch(clz=GameActionManager.class, method="getNextAction")
     public static class Trigger {
-        private static AbstractMonster.Intent[] excludedIntents = new AbstractMonster.Intent[]{AbstractMonster.Intent.ATTACK, AbstractMonster.Intent.ATTACK_BUFF, AbstractMonster.Intent.ATTACK_DEBUFF, AbstractMonster.Intent.ATTACK_DEFEND};
+        private static AbstractMonster.Intent[] excludedIntents = new AbstractMonster.Intent[]{AbstractMonster.Intent.ATTACK};
+        private static AbstractMonster.Intent[] halfIntents = new AbstractMonster.Intent[]{AbstractMonster.Intent.ATTACK_BUFF, AbstractMonster.Intent.ATTACK_DEBUFF, AbstractMonster.Intent.ATTACK_DEFEND};
         
         @SpireInsertPatch(rloc=210, localvars={"m"})
         public static void Insert(GameActionManager __instance, AbstractMonster m) {
-            for (AbstractMonster.Intent i : excludedIntents)
-                if (m.intent == i)
-                    return;
-            if (AbstractDungeon.player.hasPower(POWER_ID))
-                ((Spines)AbstractDungeon.player.getPower(POWER_ID)).trigger(m);
+            if (AbstractDungeon.player.hasPower(POWER_ID)) {
+                for (AbstractMonster.Intent i : excludedIntents)
+                    if (m.intent == i)
+                        return;
+                for (AbstractMonster.Intent i : halfIntents)
+                    if (m.intent == i) {
+                        ((Spines)AbstractDungeon.player.getPower(POWER_ID)).trigger(m, HALF_ATTACK_DIV);
+                        return;
+                    }
+                ((Spines)AbstractDungeon.player.getPower(POWER_ID)).trigger(m, 1);
+            }
         }
     }
 }
